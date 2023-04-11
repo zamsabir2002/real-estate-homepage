6898f521577caade0d8de6c950a292b7357924a3
Zameet Sabir <sabirzameet2@gmail.com>
2023-04-11 12:39:51 +0500

    Clean Up

--- src/app/page.js
+++ src/app/page.js
@@ -22,17 +22,3 @@ export default function Home() {
     </div>
   )
 }
-
-// Fetching data from the JSON file
-// import fsPromises from 'fs/promises';
-// import path from 'path'
-// export async function getStaticProps() {
-//   const filePath = path.join(process.cwd(), 'data.json');
-//   const jsonData = await fsPromises.readFile(filePath);
-//   const objectData = JSON.parse(jsonData);
-
-//   return {
-//     props: objectData
-//   }
-// }
-
